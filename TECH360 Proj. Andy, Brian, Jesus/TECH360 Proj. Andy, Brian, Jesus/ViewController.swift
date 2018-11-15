647a7958bf7bd95353625de7b5091cae67950604
Dewenye <32686675+Dewenye@users.noreply.github.com>
2018-11-15 17:56:49 -0500

    OUR GOOD APP

---
+++ TECH360 Proj. Andy, Brian, Jesus/TECH360 Proj. Andy, Brian, Jesus/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TECH360 Proj. Andy, Brian, Jesus
+//
+//  Created by Code Next Student on 11/14/18.
+//  Copyright © 2018 Code Next Student. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+class ViewController: UIViewController {
+
+    @IBAction func add(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func input(_ sender: UITextField) {
+        = UITextField()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+}
+
